99acdb885b5e9fb90dd7b4b108f2deb06737654d
monirshadkani <152501836+monirshadkani@users.noreply.github.com>
2024-11-19 17:09:25 +0100

    Start project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="w-screen h-screen flex">
+      <div className="flex flex-col flex-1 bg-green-500 justify-center items-center">
+          <p className="text-3xl font-bold" id="cookie_counter">0 Cookies</p>
+          <p className="text-3xl font-bold" id="cookie_per_second">Y cookies par seconde</p>
+          <img id="cookie_image" src="./cookie.png" className="h-80 mt-20 hover:h-96 cursor-pointer transition-all duration-300" alt="Image de cookie" />
+      </div>
+      <div className="flex flex-col flex-1 bg-yellow-500">
+          <button id="reset-button" className="rounded m-3 p-3 bg-red-100 hover:bg-red-500 hover:text-red-900 hover:font-bold transition-all duration-300 ">🗑️ Relancer le jeu</button>
+      </div>
+      <div className="flex flex-col flex-1 bg-red-500">
+          <div id="shop-container" className="flex flex-col gap-3 p-3">
+          </div>
+      </div>
+  </div>
   );
 }
 
